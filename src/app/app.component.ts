7d52482a79658fec943b895f2e5784cbfb08cd78
Tiep Phan <tieppt12@gmail.com>
2016-09-27 18:39:08 +0700

    done lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './shared/index';
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WikipediaService]
 })
 export class AppComponent {
-  title = 'app works!';
+  items: Array<string>;
+  term$ = new Subject<string>();
+  constructor(private service: WikipediaService) {
+    this.term$.subscribe((term) => this.search(term));
+  }
+  search(term: string) {
+    this.service.search(term)
+      .subscribe(res => this.items = res);
+  }
 }
